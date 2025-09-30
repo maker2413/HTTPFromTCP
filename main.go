1ee9713be40f1b5ca1e13355c7ed412060ac23d5
maker2413 <maker2413@gmail.com>
2025-09-29 19:04:20 -0500

    read text and breaking into chunks

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const inputFilePath = "messages.txt"
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesChan := make(chan string)
+
+	go func() {
+		defer close(linesChan)
+		defer f.Close()
+
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					linesChan <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				break
+			}
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				linesChan <- currentLineContents + parts[i]
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+
+	return linesChan
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
+	}
 }
